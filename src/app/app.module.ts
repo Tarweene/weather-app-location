ef71f933912ba63942684faf4f9b0e3b2c0912ab
ZuzanaMrazova1 <zuzana.mrazova1@ibm.com>
2022-06-20 20:22:54 +0200

    header added; weather-app component initial layout added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { WeatherAppComponent } from './weather-app/weather-app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherAppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
